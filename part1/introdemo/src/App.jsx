fcccedb453241d3e87c329b195a5f6ead0a69fc7
Hrushi1812 <hrushikesh1812@gmail.com>
2025-08-16 20:27:36 +0530

    done with ex1.5

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -4,36 +4,34 @@ const Part = ({ part }) => (
   </p>
 )
 
-const Content = ({ part1, part2, part3 }) => (
+const Content = ({ parts }) => (
   <div>
-    <Part part={part1} />
-    <Part part={part2} />
-    <Part part={part3} />
+    {parts.map((part, index) => (<Part key={index} part={part} />))}
   </div>
 )
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 8
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <h1>{course}</h1>
-
-      <Content part1={part1} part2={part2} part3={part3} />
-
-      <p>Number of exercises {part1.exercises + part2.exercises + part3.exercises}</p>
+      <Content parts={parts} />
+      <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
     </div>
   )
 }
