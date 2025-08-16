63411aa69a2efd285e4573b9970fbb657f13ab93
Hrushi1812 <hrushikesh1812@gmail.com>
2025-08-16 20:27:54 +0530

    done with ex1.5

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -11,28 +11,31 @@ const Content = ({ parts }) => (
 )
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 8
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 8
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <h1>{course}</h1>
-      <Content parts={parts} />
-      <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
+      <h1>{course.name}</h1>
+      <Content parts={course.parts} />
+      <p>Number of exercises {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}</p>
     </div>
   )
 }
+
 export default App
