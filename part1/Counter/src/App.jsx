0c916cd1d6bbf171520d05f952e099a7181a3365
Hrushi1812 <hrushikesh1812@gmail.com>
2025-08-20 19:17:23 +0530

    done with ex 1.7

--- part1/Counter/src/App.jsx
+++ part1/Counter/src/App.jsx
@@ -3,12 +3,20 @@ import { useState } from 'react'
 const Header = () => <h1>give feedback</h1>
 
 const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad
+
+  const average = (good - bad) / all
+  const positive = (good / all) * 100
+
   return (
     <div>
       <h1>statistics</h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {good + neutral + bad}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
     </div>
   )
 }
