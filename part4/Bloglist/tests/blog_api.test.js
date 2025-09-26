827b388fa21bb3f3a9b9499badabecd18989e87f
Hrushi1812 <hrushikesh1812@gmail.com>
2025-09-26 16:44:16 +0530

    done with ex4.10

--- part4/Bloglist/tests/blog_api.test.js
+++ part4/Bloglist/tests/blog_api.test.js
@@ -29,6 +29,39 @@ test('all blogs are returned', async () => {
   assert.strictEqual(response.body.length, 2)
 })
 
+test('unique identifier property of blogs is named id', async () => {
+  const response = await api.get('/api/blogs')
+  const blogs = response.body
+
+  blogs.forEach(blog => {
+    assert.ok(blog.id, 'blog.id is missing')
+    assert.strictEqual(blog._id, undefined, 'blog._id should not be returned')
+  })
+})
+
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: 'New blog post',
+    author: 'Charlie',
+    url: 'http://example.com/3',
+    likes: 5,
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+  const blogs = response.body
+
+  assert.strictEqual(blogs.length, initialBlogs.length + 1)
+
+  const titles = blogs.map(b => b.title)
+  assert.ok(titles.includes('New blog post'))
+})
+
 after(async () => {
   await mongoose.connection.close()
 })
